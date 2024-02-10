fbed129ac4e00c6bbba6d6212c70ea0fb2d0fbee
= <1ahmed2adil3awan@gmail.com>
2024-02-11 04:11:51 +0500

    namespace issue

--- bot-app/src/lib/utils.ts
+++ bot-app/src/lib/utils.ts
@@ -7,6 +7,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function convertToAscii(inputString: string) {
   //remove non ascii characters
-  const asciiString = inputString.replace(/[^\x00-\x7F]/g, "");
+  const asciiString = inputString
+    .replaceAll(/[^\x00-\x7F]/g, "")
+    .replaceAll(" ", "");
   return asciiString;
 }
